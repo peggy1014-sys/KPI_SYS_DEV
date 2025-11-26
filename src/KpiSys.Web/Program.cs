ada1fc3cc345dc3641492a9b5526399e5211607e
peggy1014-sys <peggy1014@gmail.com>
2025-11-26 13:54:07 +0800

    Add ASP.NET Core MVC solution scaffold

---
+++ src/KpiSys.Web/Program.cs
@@ -0,0 +1,34 @@
+using KpiSys.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+namespace KpiSys.Web
+{
+    // Used to anchor assembly-level attributes or shared constants later.
+    public partial class Program { }
+}
